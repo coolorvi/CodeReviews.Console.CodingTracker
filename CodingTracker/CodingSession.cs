ac576a330c9a391968530a15b7790d28ea91f797
CoolorVi <lizavetaaaaaaaaa@yandex.ru>
2025-12-18 14:59:50 +0300

    add model

---
+++ CodingTracker/CodingSession.cs
@@ -0,0 +1,11 @@
+﻿namespace CodingTracker
+{
+    internal class CodingSession
+    {
+        public required int ID { get; set; }
+        public required string StartTime { get; set; }
+        public required string EndTime { get; set; }
+        public required string Duration { get; set; }
+
+    }
+}
