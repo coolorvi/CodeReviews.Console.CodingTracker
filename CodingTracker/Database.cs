1652a95276e88eac2bce91afe8f1a51655bc38b8
CoolorVi <lizavetaaaaaaaaa@yandex.ru>
2025-12-30 14:25:13 +0300

    refactor config

---
+++ CodingTracker/Database.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+
+namespace CodingTracker
+{
+    internal class Database
+    {
+        public void Initialize()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            if (configuration.GetConnectionString("Default") == null)
+            {
+                AnsiConsole.MarkupLine("[red]Oops! Connection string not found.[/]");
+            }
+            string connectionString = configuration.GetConnectionString("Default");
+        }
+    }
+}
